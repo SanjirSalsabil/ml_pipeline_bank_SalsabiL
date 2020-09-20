01f213dca3b62d0cccd05b57d04353704e47c122
SANJIR <sanjirsalsabil2@gmail.com>
2020-09-20 10:01:03 +0600

    Update split_dataset.py

--- split_dataset.py
+++ split_dataset.py
@@ -5,26 +5,21 @@ import pandas as pd
 import os
 import re
 import numpy as np
-from scipy import stats
-from pandas import set_option
-from sklearn.impute import SimpleImputer
-from collections import Counter
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv(r"/Users/sanjirsalsabil/Desktop/bank.csv")
 df.head()
+df_train,df_test = train_test_split(df, test_size=0.35, random_state=100)
 
-from sklearn.model_selection import train_test_split
+df_train.to_csv("bank_training.csv",index=False)
+df_test.to_csv("bank_testing.csv",index=False)
 
-x = variable[['age', 'marital']]
-y = variable['education']
+df_train.nunique() 
 
-import sklearn.model_selection as model_selection
-x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, train_size=0.65,test_size=0.35, 
-                                                                    random_state=101)
-print ("x_train: ", x_train)
-print ("y_train: ", y_train)
-print("x_test: ", x_test)
-print ("y_test: ", y_test)
+df_train.info()
+
+set_option('display.width', 100)
+set_option('precision', 3)
+correlations = df.corr(method='pearson')
+print(correlations)
